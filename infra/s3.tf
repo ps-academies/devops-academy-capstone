ab65429f5fa4d94ba2435b40035cc72f8224d6d0
Parker Johansen <parker-johansen@pluralsight.com>
2021-12-14 12:03:15 -0700

    (infra,ci): remove solution
    Co-Authored-By: Dane Thurber <dane-thurber@pluralsight.com>

--- infra/s3.tf
+++ infra/s3.tf
@@ -6,39 +6,12 @@ resource "aws_s3_bucket" "frontend" {
   #checkov:skip=CKV_AWS_20:Website should be publicly accessible
   #checkov:skip=CKV_AWS_21:Versioning of websited is handled through git
   #checkov:skip=CKV_AWS_145:Don't encrypt publicly accessible website
-  bucket = "${var.project_name}-${random_uuid.random_id.id}"
-  acl    = "public-read"
-
-  force_destroy = true
-
-  logging {
-    target_bucket = aws_s3_bucket.logging.id
-    target_prefix = "${var.project_name}-s3-frontend"
-  }
-
-  website {
-    index_document = "index.html"
-    error_document = "404.html"
-  }
 }
 
 resource "aws_s3_bucket_policy" "frontend" {
-  bucket = aws_s3_bucket.frontend.bucket
-  policy = data.aws_iam_policy_document.frontend.json
 }
 
 data "aws_iam_policy_document" "frontend" {
-  statement {
-    sid       = "PublicReadGetObject"
-    actions   = ["s3:GetObject"]
-    resources = ["arn:aws:s3:::${aws_s3_bucket.frontend.bucket}/*"]
-
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-
-  }
 }
 
 resource "aws_s3_bucket_public_access_block" "frontend" {
@@ -46,10 +19,6 @@ resource "aws_s3_bucket_public_access_block" "frontend" {
   #checkov:skip=CKV_AWS_54:Website should be publicly accessible
   #checkov:skip=CKV_AWS_55:Website should be publicly accessible
   #checkov:skip=CKV_AWS_56:Website should be publicly accessible
-  bucket = aws_s3_bucket.frontend.id
-
-  block_public_acls   = false
-  block_public_policy = false
 }
 
 
